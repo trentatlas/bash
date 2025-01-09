8e5133b63cfbb5924b6cb2b210f5487780da5dfe
trentunixatlas <trentunix@gmail.com>
2025-01-09 12:33:01 -0500

    LESSON 0

---
+++ LESSON_00.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export PATH=/opt/trent/bin:/opt/trent/sbin:/home/trent/.local/bin:/home/trent/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+
+#set -f
+#set -v
+#set -x
+
+TIME=`date`
+echo "Hello $USER"  
+echo "time and date:  $TIME"
